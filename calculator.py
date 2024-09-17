2e49bc0749ff3e9f68e490f3b147d2b639fd1137
b3zlikiy <krykov2004@mail.ru>
2024-09-17 23:03:46 +0300

    Добавлен расчет лифта

--- calculator.py
+++ calculator.py
@@ -1,13 +1,18 @@
 import math
 
 #Метод рассчета суммы
-def Ccalculator(x, y):
+def Ccalculator(x, y, z):
+    if z == "да" or "Да":
+        l = 0
+    else:
+        l = 1
+
     multiple = math.ceil(x / 100)
     if x <= 50:
-        return 300 + 300 * multiple * (y - 1)
+        return 300 + 300 * multiple * (y - 1) * l
     elif x <= 100:
-        return 1000 + 300 * multiple * (y - 1)
+        return 1000 + 300 * multiple * (y - 1) * l
     elif x <= 300:
-        return 2000 + 300 * multiple * (y - 1)
+        return 2000 + 300 * multiple * (y - 1) * l
     elif (x <= 0 or x > 300) or y < 1:
         return "error" 
